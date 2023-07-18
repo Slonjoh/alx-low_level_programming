e413bbd26e56e6b25eb105bf1a2c75aa14aa87c1
Slonjoh <tobson11122@gmail.com>
2023-07-18 13:35:20 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Description: print alphabet of letters
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
