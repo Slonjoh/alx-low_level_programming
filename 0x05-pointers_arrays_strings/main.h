430db4f6fb511174e0d71c1285317b46c3622669
Slonjoh <tobson11122@gmail.com>
2023-07-24 15:59:13 -0700

    puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -15,4 +15,6 @@ void _putchar(char);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#endif /*MAIN_H*/
+
+#endif /* MAIN_H */
+
