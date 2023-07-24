8af3bffa7db0a8a80b5f2344862d705bc72feb8f
Slonjoh <tobson11122@gmail.com>
2023-07-24 15:31:59 -0700

    Swap int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string.
- * @s: input stringeturn.
- * Return: length of a string.
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
-int _strlen(char *s)
-{
-	int count = 0;
 
-	while (*(s + count) != '\0')
-		count++;
-	return (count);
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #define MAIN_H
 #ifndef MAIN_H
+
 #include <stdio.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,4 +15,4 @@ void _putchar(char);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#endif 
+#endif /*MAIN_H*/
