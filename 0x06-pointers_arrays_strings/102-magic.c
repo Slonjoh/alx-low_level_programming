d8a55be11115ae13b827f1cb10d450056daa3441
Slonjoh <tobson11122@gmail.com>
2023-07-27 15:05:24 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,10 +17,11 @@ int main(void)
     * - only one statement
     * - you are not allowed to code anything else than this line of code
     */
-    *(p + 5) = 98; /* The line of code that prints 98*/
+    *(p + 7 - (p - &a[2])) = 98; /* The line of code that prints 98*/
 
     /* ...so that this prints 98\n */
     printf("a[2] = %d\n", a[2]);
     return (0);
 }
+
 
