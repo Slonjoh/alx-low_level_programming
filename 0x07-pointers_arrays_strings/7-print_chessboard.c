74b9e14f9e104429245d5c29dac9ec0917bf52fe
Slonjoh <tobson11122@gmail.com>
2023-08-01 16:17:39 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,9 +8,13 @@
 void print_chessboard(char (*a)[8])
 
 {
-	for (int i = 0; i < 8; i++)
+	int i;
+
+	for (i = 0; i < 8; i++)
     {
-	    for (int j = 0; j < 8; j++)
+	    int j;
+
+	    for (j = 0; j < 8; j++)
 	    {
 		    putchar(a[i][j]);
 		    putchar(' ');
