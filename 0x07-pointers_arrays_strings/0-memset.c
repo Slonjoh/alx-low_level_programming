c61e14023075b727c15ffdbcf66613595f2b145b
Slonjoh <tobson11122@gmail.com>
2023-07-31 17:26:07 -0700

    meme

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,9 +10,22 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	for (unsigned int i = 0; i < n; i++)
+	unsigned int i;
+	for (i = 0; i < n; i++)
 	{
 		*(s + i) =  b;
 	}
 	return (s);
+}
+int main()
+{
+	char arr[10];
+	char *result = _memset(arr, 'A', 10);
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%p ", (void*)&result[i]);
+	}
+
+	return (0);
 }
