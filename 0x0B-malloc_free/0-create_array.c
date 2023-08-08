ac301d07b0fa6d1768aafc2eeb6bf1612b742b03
Slonjoh <tobson11122@gmail.com>
2023-08-08 15:17:35 -0700

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,13 +11,13 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *ptr;
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
-	char *ptr;
-	unsigned int i;
 
 	ptr = malloc(size * sizeof(c));
 
