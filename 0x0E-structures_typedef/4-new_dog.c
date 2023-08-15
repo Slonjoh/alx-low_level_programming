8f1449f500148a5384915cb57f45cb76ad65549f
Slonjoh <tobson11122@gmail.com>
2023-08-14 17:53:59 -0700

    add new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,27 +16,43 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog_ptr;
+	int i, lname, lowner;
+
+	lname = strlen(name);
+	lowner = strlen(owner);
 
 	new_dog_ptr = malloc(sizeof(dog_t));
 	if (new_dog_ptr == NULL)
 		return (NULL);
 
-	new_dog_ptr->name = strdup(name);
+	new_dog_ptr->name = malloc(lname + 1);
 	if (new_dog_ptr->name == NULL)
 	{
 		free(new_dog_ptr);
 		return (NULL);
 	}
 
-	new_dog_ptr->owner = strdup(owner);
+	for (i = 0; i < lname; i++)
+		new_dog_ptr->name[i] = name[i];
+
+	new_dog_ptr->name[i] = '\0';
+
+	new_dog_ptr->age = age;
+
+	new_dog_ptr->owner = malloc(lowner + 1);
+
 	if (new_dog_ptr->owner == NULL)
 	{
 		free(new_dog_ptr->name);
 		free(new_dog_ptr);
+
 		return (NULL);
 	}
 
-	new_dog_ptr->age = age;
+	for (i = 0; i < lowner; i++)
+		new_dog_ptr->owner[i] = owner[i];
+
+	new_dog_ptr->owner[i] = '\0';
 
 	return (new_dog_ptr);
 }
