f9955b43524acafd927a82e9d098ecd2c1c48182
Slonjoh <tobson11122@gmail.com>
2023-08-17 06:50:17 -0700

    Print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,14 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
+
+
 /**
  * print_all - Prints arguments based on the format provided.
  * @format: A list of types of arguments passed to the function
