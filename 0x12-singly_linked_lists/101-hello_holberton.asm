b4ff9b9dd1b49bedad2810d209377d96213bdfeb
Slonjoh <tobson11122@gmail.com>
2023-08-24 11:47:43 -0700

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,30 +1,15 @@
-section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 0
-    newline db 10, 0
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-    global main
-
+	SECTION .text
+	extern printf
+	global main
 main:
-    push rax
-    push rbx
-    push rcx
-    push rdx
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-    ; Print "Hello, Holberton"
-    mov rdi, format
-    mov rsi, hello
-    call printf
-
-    ; Print newline
-    mov rdi, format
-    mov rsi, newline
-    call printf
-
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
-
-    ret
+	mov eax, 0
+	ret
