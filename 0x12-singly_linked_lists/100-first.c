8f780ce98c6715fbc74c94e267bb4145ad099feb
Slonjoh <tobson11122@gmail.com>
2023-08-24 11:41:18 -0700

    Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) pre_main_message(void);
+
+/**
+ * pre_main_message - Prints a message before the main function is executed.
+ */
+
+void pre_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
