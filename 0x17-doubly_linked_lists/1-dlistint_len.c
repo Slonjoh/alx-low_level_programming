059701a408a93428b8f864cdd20b1d73e8b00948
Slonjoh <tobson11122@gmail.com>
2023-10-12 05:51:09 -0700

    list len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,11 +8,12 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
+	const dlistint_t *current = h;
 
-	while (h)
+	while (current != NULL)
 	{
 		count++;
-		h = h->next;
+		current = current->next;
 	}
 
 	return (count);
