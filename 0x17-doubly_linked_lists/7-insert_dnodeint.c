835bf0e8efbcab08bcf17acce41598c037db126e
Slonjoh <tobson11122@gmail.com>
2023-10-12 08:35:01 -0700

    betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,11 +15,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		return NULL;
-
+		return (NULL);
 	new_node->n = n;
 
 	if (idx == 0)
@@ -51,6 +49,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (current->next)
 		current->next->prev = new_node;
 	current->next = new_node;
-
 	return (new_node);
 }
