6fb3b793c1e5aed5c11ddda1c15f8bee1f272be4
Slonjoh <tobson11122@gmail.com>
2023-10-16 08:10:19 -0700

    ramdom

---
+++ 0x18-dynamic_libraries/nrandom.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * generate_random_number - Generate a random number.
+ *
+ * This function generates a random number using the rand() function.
+ *
+ * Return: An integer representing the generated random number.
+ */
+int generate_random_number()
+{
+	return rand();
+}
